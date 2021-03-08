1d2843cde6b2480cd2d12b8a98110d04471e11be
bluayer <ijacsong98@gmail.com>
2021-03-08 20:37:02 +0900

    feat: node에서 잘 되는 거 확인 완료

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,17 @@ function generateConfig(name) {
   const compress = name.indexOf('min') > -1;
   const config = {
     entry: './lib/index.ts',
+    externals: {
+      axios: 'axios'
+    },
     output: {
       path: __dirname + '/dist/',
       filename: name + '.js',
       sourceMapFilename: name + '.map',
       library: 'cooparser',
       libraryTarget: 'umd',
-      globalObject: 'this'
+      umdNamedDefine: true,
+      globalObject: 'typeof self !== \'undefined\' ? self : this',
     },
     module: {
         rules: [
