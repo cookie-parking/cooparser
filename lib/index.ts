82c07996b3b126788254a037342f2154e609afc6
SongJungwoo <ijacsong98@gmail.com>
2021-03-09 18:15:50 +0900

    hotfix: 테스트 코드 삭제

--- lib/index.ts
+++ lib/index.ts
@@ -3,10 +3,4 @@ import CooparserImpl from './cooparser';
 
 const cooparser = CooparserImpl.getInstance();
 
-const a = async () => {
-    const data = await cooparser.parseList(["https://google.com", "https://youtube.com", "https://www.marvel.com/404"]);
-    console.log(data);
-}
-
-a();
 export { ParseResponse, cooparser };
